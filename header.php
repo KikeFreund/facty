19ebcb18317f55396296d2835ddaf571fff6d191
KikeFreund <lozkikin@gmail.com>
2025-06-02 16:53:59 -0600

    Estructura cliente

--- header.php
+++ header.php
@@ -17,3 +17,6 @@ $tipoUsuario=$_SESSION['tipoUsuario'];
   <!-- <link href="assets/css/styles.css" rel="stylesheet"> -->
   <link href="assets/css/estilo.css" rel="stylesheet">
 </head>
+
+<!-- Bootstrap 5 JS Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
