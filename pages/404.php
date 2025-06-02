0e0b6fce27cc1c0a1af3d867df2d576ae8e24b87
KikeFreund <lozkikin@gmail.com>
2025-06-02 17:42:05 -0600

    Estructura cliente

--- pages/404.php
+++ pages/404.php
@@ -14,7 +14,7 @@ require('route.php');
 <body class="">
 
   <div class="text-center">
-  
+  <img src="assets/img/gato.jpg" alt="" class="img-fluid">
     <h1 class="display-1 fw-bold text-primary">  <i class="fas fa-file-invoice"></i>404</h1>
     <p class="fs-3"> <span class="text-danger">Ups!</span> Página no encontrada.</p>
     <p class="lead">
