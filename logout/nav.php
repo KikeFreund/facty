f49eb092ccf1a930ae02d4ffed26ba49b7cc90e1
KikeFreund <lozkikin@gmail.com>
2025-06-02 16:18:21 -0600

    Estructura cliente

--- logout/nav.php
+++ logout/nav.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container">
-        <p class="navbar-brand" href="/">
+        <a class="navbar-brand" href="/">
             <i class="fas fa-file-invoice"></i>
             FactyFlow
-</p>
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
