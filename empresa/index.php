aed4a4879aae6cb29787c760139c05de7f870ade
KikeFreund <lozkikin@gmail.com>
2025-06-19 14:36:49 -0600

    Punto de inicio

--- empresa/index.php
+++ empresa/index.php
@@ -1,12 +1,4 @@
-<?php include 'nav.php'; ?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Panel de Empresa</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
+
 <body class="bg-light">
   <div class="container mt-5">
     <h1 class="mb-4 text-center">Bienvenido al Panel de Empresa</h1>
@@ -52,4 +44,4 @@
   <?php include 'footer.php'; ?>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-</html>
+
