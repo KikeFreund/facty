03510ce8335ef06a930a2c204119648966bf6c6e
KikeFreund <lozkikin@gmail.com>
2025-06-19 14:38:31 -0600

    Punto de inicio

--- empresa/index.php
+++ empresa/index.php
@@ -8,7 +8,7 @@
           <div class="card-body text-center">
             <h5 class="card-title">Ver Tickets</h5>
             <p class="card-text">Consulta los tickets que te han dado los clientes.</p>
-            <a href="ver-tickets.php" class="btn btn-primary">Ir a Tickets</a>
+            <a href="ver-tickets" class="btn btn-primary">Ir a Tickets</a>
           </div>
         </div>
       </div>
@@ -17,7 +17,7 @@
           <div class="card-body text-center">
             <h5 class="card-title">Registrar Datos Fiscales</h5>
             <p class="card-text">Registra los datos fiscales de tu empresa para facturación.</p>
-            <a href="registrar-datos-fiscales.php" class="btn btn-primary">Registrar</a>
+            <a href="registrar-datos-fiscales" class="btn btn-primary">Registrar</a>
           </div>
         </div>
       </div>
@@ -26,7 +26,7 @@
           <div class="card-body text-center">
             <h5 class="card-title">Editar Datos</h5>
             <p class="card-text">Edita el nombre, dirección y teléfono de tu empresa.</p>
-            <a href="editar-datos.php" class="btn btn-primary">Editar</a>
+            <a href="editar-datos" class="btn btn-primary">Editar</a>
           </div>
         </div>
       </div>
@@ -35,13 +35,13 @@
           <div class="card-body text-center">
             <h5 class="card-title">Lector QR</h5>
             <p class="card-text">Accede al lector de códigos QR para procesar tickets.</p>
-            <a href="lector-qr.php" class="btn btn-primary">Lector QR</a>
+            <a href="lector-qr" class="btn btn-primary">Lector QR</a>
           </div>
         </div>
       </div>
     </div>
   </div>
-  <?php include 'footer.php'; ?>
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
--- empresa/nav.php
+++ empresa/nav.php
@@ -3,29 +3,29 @@
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Empresa</a>
+    <a class="navbar-brand" href="index">Empresa</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto">
         <li class="nav-item">
-          <a class="nav-link" href="index.php">Inicio</a>
+          <a class="nav-link" href="index">Inicio</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="ver-tickets.php">Ver Tickets</a>
+          <a class="nav-link" href="ver-tickets">Ver Tickets</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="registrar-datos-fiscales.php">Registrar Datos Fiscales</a>
+          <a class="nav-link" href="registrar-datos-fiscales">Registrar Datos Fiscales</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="editar-datos.php">Editar Datos</a>
+          <a class="nav-link" href="editar-datos">Editar Datos</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="lector-qr.php">Lector QR</a>
+          <a class="nav-link" href="lector-qr">Lector QR</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../logout/index.php">Cerrar Sesión</a>
+          <a class="nav-link" href="../logout/index">Cerrar Sesión</a>
         </li>
       </ul>
     </div>
