16f050042c5674621b843410ba5d5fcdc918076a
KikeFreund <lozkikin@gmail.com>
2025-06-23 17:18:18 -0600

    Punto de inicio

--- empresa/nav.php
+++ empresa/nav.php
@@ -25,7 +25,7 @@
           <a class="nav-link" href="lector-qr">Lector QR</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../index">Cerrar Sesión</a>
+          <a class="nav-link" href="funciones/logout.php">Cerrar Sesión</a>
         </li>
       </ul>
     </div>
