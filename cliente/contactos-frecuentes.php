73ce6995494f8841e3e3b656010a9dbcf32fa014
KikeFreund <lozkikin@gmail.com>
2025-08-21 18:41:21 -0600

    Contactos

--- cliente/contactos-frecuentes.php
+++ cliente/contactos-frecuentes.php
@@ -35,8 +35,7 @@ $estadisticas = obtenerEstadisticasContactos($_SESSION['id_usuario']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contactos Frecuentes - Facty</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
+
 </head>
 <body class="bg-light">
 
