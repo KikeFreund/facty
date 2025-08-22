093ca55e6603596b6f492b0d4d47af8495936854
KikeFreund <lozkikin@gmail.com>
2025-08-21 18:42:41 -0600

    Contactos

--- cliente/generar-ticket.php
+++ cliente/generar-ticket.php
@@ -238,7 +238,7 @@ $result_metodos_pago =  $conn->query($query_metodos_pago);
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
     <script>
     let stream = null;
     let fotoCapturada = null;
